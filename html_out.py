9f575c710f74554c0d1a6731587d6ea863a3017f
White <white@chbrath.de>
2023-02-19 17:57:26 +0100

    https://github.com/derwhite/whitez_mplus/issues/29  Finally it works. Maybe wanna change it to only active one row.

--- html_out.py
+++ html_out.py
@@ -78,7 +78,7 @@ def gen_score_table(players, inis, colors, isTyrannical):
 		tier = p.get_tier_items()
 		#----------------------------------
 		# Create Player Line on Website !!
-		str_html += f'<tr>\n'
+		str_html += f'<tr onclick="highlightRow(this)">\n'
 		str_html += f'<td title="Last Update: {p.days_since_last_update()} days ago&#10;{tier}"><a href="{p.profile_url()}" target="_blank"><img src="{p.thumbnail_url()}" width="40" height="40" style="float:left"></a><p style="font-size:{mainSize}px;color:{p.class_color};padding:10px;margin:0px;text-align:left">&emsp;{p.name}</p></td>\n'
 		str_html += f'<td><span style="color: {p.class_color}">{p.ilvl}</span></td>'
 		sum = p.score  # Player Score
@@ -129,7 +129,7 @@ def gen_weekly(players, inis, colors, weekly):
 		# Count Tier Items an Build a string
 		tier = p.get_tier_items()
 		# ----------------------------------
-		str_html += f'<tr>\n'
+		str_html += f'<tr onclick="highlightRow(this)">\n'
 		str_html += f'<td title="Last Update: {p.days_since_last_update()} days ago&#10;{tier}"><a href="{p.profile_url()}" target="_blank"><img src="{p.thumbnail_url()}" width="40" height="40" style="float:left"></a><p style="color:{p.class_color};padding:10px;margin:0px;text-align:left;">&emsp;{p.name}</p></td>\n'
 		str_html += f'<td><span style="color: {p.class_color}">{p.ilvl}</span></td>'
 		# --------- Show Left Instances -------
