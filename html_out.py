5d9670dad1b37f88786f8a6a4a259c8fa169f961
Kaeseknacker <Raphaelspraul@hotmail.de>
2023-02-24 15:32:45 +0100

    move t-set tt to ilvl column in general tab

--- html_out.py
+++ html_out.py
@@ -75,8 +75,8 @@ def gen_general_tab(players):
 		# ----------------------------------
 		# Create Player Line on Website !!
 		str_html += f'<tr class="player_row" onclick="highlightRow(this)">\n'
-		str_html += f'<td title="Last Update: {p.days_since_last_update()} days ago&#10;{tier}"><a href="{p.profile_url()}" target="_blank"><img src="{p.thumbnail_url()}" width="35" height="35" style="float:left"></a><p style="color:{p.class_color};padding:6px;margin:0px;text-align:left">&emsp;{p.name}</p></td>\n'
-		str_html += f'<td class="td_ilvl"><span style="color: {p.class_color}">{p.ilvl}</span></td>'
+		str_html += f'<td title="Last Update: {p.days_since_last_update()} days ago"><a href="{p.profile_url()}" target="_blank"><img src="{p.thumbnail_url()}" width="35" height="35" style="float:left"></a><p style="color:{p.class_color};padding:6px;margin:0px;text-align:left">&emsp;{p.name}</p></td>\n'
+		str_html += f'<td class="td_ilvl" title="{tier}"><span style="color: {p.class_color}">{p.ilvl}</span></td>'
 		str_html += f'</tr>\n'
 	str_html += f'</table>\n'
 	return str_html
